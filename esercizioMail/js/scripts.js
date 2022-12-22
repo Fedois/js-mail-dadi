4af790c6fd672bb83f317133492c0c9b708563ca
Fedois <fedefox14@gmail.com>
2022-12-22 16:10:54 +0100

    inizializzato progetto: email

---
+++ esercizioMail/js/scripts.js
@@ -0,0 +1,15 @@
+const iscritti = ["Luca@mail.com", "Marco@mail.com", "Paolo@mail.com"];
+console.log('iscritti', iscritti, typeof iscritti);
+
+const utente = prompt('inserisci la tua email per accedere:');
+console.log('email inserita: ', utente)
+
+if(utente == iscritti[0] || iscritti[1] || iscritti[2]){
+    alert('puoi accedere')
+}
+else{
+    alert('non sei nella lista. non puoi accedere')
+}
+
+
+
